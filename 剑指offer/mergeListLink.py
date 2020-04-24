2b797a9876df07d15b038a39b2789d766c32775a
QingfengTang <2997528102@qq.com>
2020-04-24 16:23:37 +0800

    add code

--- 剑指offer/mergeListLink.py
+++ 剑指offer/mergeListLink.py
@@ -7,12 +7,16 @@ class ListNode():
         self.next = None
 
 class Merge():
-    def soluton1(self, pHead1, phead2):
+    def soluton1(self, pHead1, pHead2):
+        '''
+        非递归实现：从头开始逐次比较，将小拿出来排好，再移位，重复操作直到最后
+        '''
         tmp = ListNode(0)
         pHead = tmp
         while pHead1 and pHead2:
             if pHead1.val < pHead2.val:
                 tmp.next = pHead1
+                # phead1位置更新为下一位
                 pHead1 = pHead1.next
             else:
                 tmp.next = pHead2
@@ -22,6 +26,22 @@ class Merge():
             tmp.next = pHead2
         if not pHead2:
             tmp.next = pHead1
-        return pHead.next            
+        return pHead.next
+
+    def soluton2(self, pHead1, pHead2):
+        '''
+        递归实现
+        '''
+        if not pHead1:
+            return pHead2
+        if not pHead2:
+            return pHead1
+
+        if pHead1.val > pHead2.val:
+            pHead2.next = self.soluton2(pHead1, pHead2.next)
+            return pHead2
+        else:
+            pHead1.next = self.soluton2(pHead1.next, pHead2)
+            return pHead1            
 
         
